ece3ea28d0dd3d1659cbc2e45cace00bcb470677
schin <schin@uchicago.edu>
2026-06-18 17:05:09 -0400

    actual actual merge

--- frontend/src/components/ProposeActivity.jsx
+++ frontend/src/components/ProposeActivity.jsx
@@ -13,6 +13,7 @@ import CommentComposer from "./CommentComposer.jsx";
 import CommentLikeButton from "./CommentLikeButton.jsx";
 import MentionText from "./MentionText.jsx";
 import { relativeTime } from "../utils/time.js";
+import { cx } from "../utils/classNames.js";
 import styles from "./styling/ProposeActivity.module.css";
 
 const COLLAPSED_COMMENT_LIMIT = 10;
